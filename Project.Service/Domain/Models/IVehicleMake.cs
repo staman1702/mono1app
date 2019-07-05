1e61cf9239cf66c876ca434013bf496dfaa8e459
staman1702 <staman1702@gmail.com>
2019-07-05 17:12:07 +0200

    05-07-2019-I

---
+++ Project.Service/Domain/Models/IVehicleMake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Service.Domain.Models
+{
+    public interface IVehicleMake
+    {        
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Abrv { get; set; }
+        ICollection<IVehicleModel> VehicleModel { get; set; }
+    }
+}
+}
---
+++ Project.Service/Domain/Models/IVehicleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Service.Domain.Models
+{
+    public interface IVehicleModel
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Abrv { get; set; }
+
+        Guid VehicleMakeId { get; set; }
+        IVehicleMake VehicleMake { get; set; }
+    }
+}
